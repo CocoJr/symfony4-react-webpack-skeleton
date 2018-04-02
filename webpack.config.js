12aae3139727f0548555bea594cbfc64bff39108
CocoJr <thibaultcolette06@hotmail.fr>
2018-04-02 11:52:06 +0200

    Fix webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -16,94 +16,114 @@ const ABSOLUTE_ASSETS_DIR = path.resolve(__dirname, ASSETS_DIR);
 
 let cssLoaders = [
     {
-        loader: 'css-loader',
-        options: {
-            importLoaders: 1
-        }
-    }
+        loader: 'css-loader'
+    },
 ];
 
 let cleanOptions = {
-    root: ABSOLUTE_ASSETS_DIR,
+    root: path.resolve(__dirname, ABSOLUTE_ASSETS_DIR + '/../'),
     verbose: true,
     dry: false,
     watch: DEV,
     beforeEmit: true
 };
 
-let config = {
-    watch: DEV,
-    devtool: DEV ? "cheap-module-eval-source-map" : false,
-    entry: {
-        'react-app.js': [
-            'babel-polyfill',
-            './src/assets/react/index.js'
-        ],
-        'app.js': [
-            './src/assets/js/app.js'
-        ],
-        'app.css': [
-            './src/assets/css/app.css',
-            './src/assets/scss/app.scss'
-        ]
+let rules = {
+    'js': {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+            loader: 'babel-loader'
+        }
     },
-    output: {
-        filename: '[chunkhash:12].[name]',
-        path: ABSOLUTE_ASSETS_DIR
+    'ressources': {
+        test: /\.(png|jpg|jpeg|gif|eot|svg|woff2?|ttf)$/,
+        use: {
+            loader: 'url-loader',
+            options: {
+                limit: 8192
+            }
+        }
     },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.css$/,
-                use: cssLoaders
-            },
-            {
-                test: /\.(png|jpg|jpeg|gif|eot|svg|woff2?|ttf)$/,
-                use: {
-                    loader: 'url-loader',
-                    options: {
-                        limit: 8192
-                    }
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: [...cssLoaders, "sass-loader"]
-    			})
-			},
-		]
-	},
-	plugins: [
-        new webpack.DefinePlugin({
-            API_V1_BASE_URL: '"'+process.env.API_V1_BASE_URL+'"',
-        }),
-		new CleanWebpackPlugin([ABSOLUTE_ASSETS_DIR], cleanOptions),
-		new ExtractTextPlugin('[name]'),
-		new ManifestPlugin({
-			fileName: 'manifest.json',
-			basePath: WEB_ASSETS_DIR,
-			map: (obj) => {
-			obj.name = obj.name
-				.replace(/\.js\.js$/, '.js')
-				.replace(/\.css\.css$/, '.css');
-
-				return obj;
-			}
-		}),
-	]
+    'css': {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: "css-loader"
+        })
+    },
+    'scss': {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: [...cssLoaders, "sass-loader"]
+        })
+    },
 };
 
+let config = [
+    {
+        watch: DEV,
+        devtool: DEV ? "cheap-module-eval-source-map" : false,
+        entry: {
+            'react-app': [
+                'babel-polyfill',
+                './src/assets/react/index.js'
+            ],
+            'app': [
+                './src/assets/js/app.js'
+            ],
+        },
+        output: {
+            filename: '[chunkhash:12].[name].js',
+            path: ABSOLUTE_ASSETS_DIR + '/js',
+        },
+        plugins: [
+            new webpack.DefinePlugin({
+                API_V1_BASE_URL: '"' + process.env.API_V1_BASE_URL + '"',
+            }),
+            new ExtractTextPlugin('[name]'),
+            new CleanWebpackPlugin([ABSOLUTE_ASSETS_DIR + '/js'], cleanOptions),
+            new ManifestPlugin({
+                fileName: 'manifest.json',
+                basePath: WEB_ASSETS_DIR+'js/'
+            }),
+        ],
+        module: {
+            rules: [rules.js, rules.ressources]
+        },
+    },
+    {
+        watch: DEV,
+        devtool: DEV ? "cheap-module-eval-source-map" : false,
+        entry: {
+            'app': [
+                './src/assets/scss/app.scss'
+            ],
+            'app2': [
+                './src/assets/css/app.css'
+            ]
+        },
+        output: {
+            filename: '[chunkhash:12].[name].css',
+            path: ABSOLUTE_ASSETS_DIR + '/css',
+        },
+        plugins: [
+            new ExtractTextPlugin('[chunkhash:12].[name].css'),
+            new CleanWebpackPlugin([ABSOLUTE_ASSETS_DIR + '/css'], cleanOptions),
+            new ManifestPlugin({
+                fileName: 'manifest.json',
+                basePath: WEB_ASSETS_DIR+'css/'
+            }),
+        ],
+        module: {
+            rules: [rules.ressources, rules.scss, rules.css]
+        },
+    },
+];
+
 if (!DEV) {
-    config.plugins.push(new UglifyJSPlugin());
+    config[0].plugins.push(new UglifyJSPlugin());
 }
 
 module.exports = config;
